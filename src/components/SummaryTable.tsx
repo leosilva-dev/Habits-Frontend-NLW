53c1fedef5fd9f815a257a8a60a008a62048e596
Leonardo Silva <silvaleonardo.ti@gmail.com>
2023-01-20 18:11:39 -0300

    Improving components

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -12,26 +12,34 @@ export function SummaryTable() {
   return (
     <div className="w-full flex">
       <div className="grid grid-rows-7 grid-flow-row gap-3">
-        {weekDays.map((day, index) => (
-          <div
-            key={day + index}
-            className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
-          >
-            {day}
-          </div>
-        ))}
+        {weekDays.map((weekDay, i) => {
+          return (
+            <div
+              key={`${weekDay}-${i}`}
+              className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
+            >
+              {weekDay}
+            </div>
+          );
+        })}
       </div>
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map((date) => {
-          return <HabitDay key={date.toString()} />;
+          return (
+            <HabitDay
+              key={date.toString()}
+              amount={5}
+              completed={Math.round(Math.random() * 5)}
+            />
+          );
         })}
 
         {amountOfDaysToFill > 0 &&
-          Array.from({ length: amountOfDaysToFill }).map((_, index) => {
+          Array.from({ length: amountOfDaysToFill }).map((_, i) => {
             return (
               <div
-                key={index}
+                key={i}
                 className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
               />
             );
