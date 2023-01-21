1f8e10209991e5ee75e0530eb0b783df4ff4d661
Leonardo Silva <silvaleonardo.ti@gmail.com>
2023-01-21 01:44:16 -0300

    configuring dayjs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-// import { Habit } from "./components/Habit";
+import "./styles/global.css";
+import "./lib/dayjs";
 import { Header } from "./components/Header";
 import { SummaryTable } from "./components/SummaryTable";
-import "./styles/global.css";
+
+// import { Habit } from "./components/Habit"
 
 export function App() {
   return (
